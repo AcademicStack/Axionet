8f0f3f923bebb3042f344b005349cfefb2afd8e3
Jon <coryellj1@students.westerntc.edu>
2026-02-04 00:28:40 -0600

    added files and a folder

---
+++ image-gallery/js/image-gallery-component.js
@@ -0,0 +1,21 @@
+/**
+ * Creates an image gallery within a specified HTML element.
+ * The gallery includes navigation buttons to cycle through a list of images.
+ *
+ * @param {HTMLElement} el - The container element where the gallery will be rendered.
+ * @param {Array<{path: string, description: string}>} images - An array of image objects, where each object contains:
+ *   - `path` (string): The URL or path to the image.
+ *   - `description` (string): A textual description of the image.
+ *
+ * @example
+ * const galleryContainer = document.getElementById("gallery");
+ * const imageList = [
+ *   { path: "image1.jpg", description: "Image 1 description" },
+ *   { path: "image2.jpg", description: "Image 2 description" }
+ * ];
+ * createGallery(galleryContainer, imageList);
+ */
+
+const createGallery = (el, images) => {
+	// TODO: Put your code here
+}
